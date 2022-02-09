ef4fde7bef9ba3f8d646461c8f35f80831df04c5
snehasis <snehasisdebbarman2016@gmail.com>
2022-02-10 04:17:23 +0530

    firestore Implementation

--- src/component/Todo/Todo.js
+++ src/component/Todo/Todo.js
@@ -1,120 +1,417 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {
-  TextField,
-  Button,
-  Card,
-  IconButton,
-  CardContent,
-  Paper,
-} from "@mui/material";
+import { Button, IconButton, Paper, Checkbox, InputBase } from "@mui/material";
+import sun from "../../images/icon-sun.svg";
+import moon from "../../images/icon-moon.svg";
+import iconCheck from "../../images/icon-check.svg";
 import "./todo.css";
-import { SignalCellularNull, TextFieldsOutlined } from "@mui/icons-material";
-const axios = require("axios");
+import "./todo.scss";
+import { pink } from "@mui/material/colors";
+// import { styled } from "@mui/material/styles";
+// import DraggableList from "react-draggable-lists";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuid } from "uuid";
 
-export default function Todo(props) {
-  // const array1 = ["a", "b", "c"];
-  const [value, setValue] = React.useState({
-    name: "",
-    text: "text",
-    date: new Date().toString(),
-  });
-  const [text, setText] = useState("");
-  const [array1, setArray1] = useState([value]);
-  const [first, setfirst] = useState();
-  const [id, setid] = useState(guidGenerator);
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
-  function getId(){
-    axios
-      .get("https://www.uuidgenerator.net/api/version1")
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        setid(response.data);
-        //
-      });
-  }
-  function guidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
-
-  const handleChange = (event) => {
-
-    setText(event.target.value)
-    setValue({
-      name: id||guidGenerator,
-      text: event.target.value,
-      date: new Date()
-        .toLocaleDateString("en-us", {
-          weekday: "long",
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })
-        .toString(),
-    });
-  };
-
-  const handleClick = (event) => {
-    getId()
-    setArray1([...array1, value]);
-    setText("")
-  };
-
-  // const listItems = array1.map((item) => (
-  //   <div className="todo">
-  //     <div>
-  //       <h2 className="todoHeader">{item.name}</h2>
-  //     </div>
-  //     <div>
-  //       <p className="todoBody">{item.text}</p>
-  //     </div>
-  //     <footer className="todoFooter">{item.date}</footer>
-  //   </div>
-  // ));
-
+import firebaseApp from "../../firebaseApp";
+import { async } from "@firebase/util";
+const GradientCheckIcon = () => {
+  return (
+    <img
+      src={iconCheck}
+      style={{
+        height: "auto",
+        width: "auto",
+        background: "linear-gradient(hsl(192, 100%, 67%) , hsl(280, 87%, 65%))",
+        border: "2px solid white",
+        borderRadius: "50%",
+        padding: "5px",
+      }}
+    />
+  );
+};
+const CheckIcon = () => {
   return (
     <div>
-      <div className="add-btn"></div>
-    
-    <div >
-      
-      <TextField
-        id="todo"
-        label="Todo"
-        variant="outlined"
-        onChange={handleChange}
-        value={text}
-      />
-      <Button
-        variant="contained"
-        style={{ padding: "15px", marginLeft: "20px" }}
-        onClick={handleClick}
-        disabled={text.length===0?true:false}
-       
+      <svg
+        style={{
+          height: "auto",
+          width: "auto",
+          background: "transparent",
+          borderRadius: "55%",
+          border: "2px solid white",
+          padding: "5px",
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="9"
       >
-        <p>Submit</p>
-      </Button>
-      <h1 style={{ padding: "10px", fontSize: "2rem" }}>ToDos</h1>
-      <div className="todoContainer flex-container">
-        <>
-          {array1.map((item) => (
-            <div className="todo flex-items">
-              <div >
-                <h2 className="todoHeader">{item.name}</h2>
-              </div>
-              <div>
-                <p className="todoBody">{item.text}</p>
-              </div>
-              <footer className="todoFooter">{item.date}</footer>
-            </div>
-          ))}
-        </>
-      </div>
+        <path
+          fill="none"
+          stroke="transparent"
+          strokeWidth="2"
+          d="M1 4.304L3.696 7l6-6"
+        />
+      </svg>
+      {/* <img src={iconCheck} /> */}
     </div>
+  );
+};
+const CheckIconDark = () => {
+  return (
+    <div>
+      <svg
+        style={{
+          height: "auto",
+          width: "auto",
+          background: "transparent",
+          borderRadius: "55%",
+          border: "2px solid grey",
+          padding: "5px",
+          "input:hover ~ &": {
+            backgroundColor: "red",
+          },
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="9"
+      >
+        <path
+          fill="none"
+          stroke="transparent"
+          strokeWidth="2"
+          d="M1 4.304L3.696 7l6-6"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default function Todo({ handleLogout }) {
+  // const array1 = ["a", "b", "c"];
+  const [DarkMode, setDarkMode] = useState(false);
+  const [Todos, setTodos] = useState([]);
+  const [InputValue, setInputValue] = useState("");
+  const [dummy, setdummy] = useState("");
+  const [ActiveTodo, setActiveTodo] = useState(false);
+  const [CompletedTodo, setCompletedTodo] = useState(false);
+  const desauratedBlue = "hsl(235, 24%, 19%)";
+  const listItems = ["Entertainment"];
+  //firestore
+  const db = getFirestore();
+  useEffect(() => {
+    getTodos();
+  }, [dummy]);
+
+  //update
+  async function updateTodo(id, status, item) {
+    let st;
+    status ? (st = "completed") : (st = "active");
+    await setDoc(doc(db, "todos", id), {
+      id: id,
+      item: item,
+      status: st,
+    });
+  }
+
+  async function saveTodo(input) {
+    //const saveToFirebase = getFirestore();
+    try {
+      const docRef = await addDoc(collection(db, "todos"), {
+        id: uuid(),
+        item: input,
+        status: "active",
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  async function getTodos() {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    const saveFirebaseTodos = [];
+    querySnapshot.forEach((doc) => {
+      // console.log(`${doc.id} => ${doc.data()}`);
+      let s = [doc.id, doc.data()];
+      saveFirebaseTodos.push(s);
+    });
+    setTodos(saveFirebaseTodos);
+    console.log(Todos);
+  }
+
+  const keyPress = (e) => {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+      saveTodo(e.target.value);
+      //listItems.push(e.target.value);
+      setInputValue("");
+      setdummy(e.target.value);
+      // put the login here
+    }
+  };
+
+  const ListItemContainer = ({ uid, todoItem, todoStatus }) => {
+    return (
+      <div style={{ borderTop: "0.1px solid grey" }}>
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+          }}
+          style={
+            !DarkMode
+              ? {
+                  background: desauratedBlue,
+                  color: "white",
+                }
+              : {
+                  background: "white",
+                  color: desauratedBlue,
+                }
+          }
+        >
+          <IconButton sx={{ p: "10px" }} aria-label="menu">
+            <Checkbox
+              icon={DarkMode ? <CheckIconDark /> : <CheckIcon />}
+              checkedIcon={<GradientCheckIcon />}
+              style={!DarkMode ? { color: "white" } : { color: desauratedBlue }}
+              onChange={(e) => updateTodo(uid, e.target.checked, todoItem)}
+            />
+          </IconButton>
+          <div
+            float="left"
+            // className={DarkMode ? "textfield textfield-dark" : "textfield"}
+            style={
+              !DarkMode
+                ? {
+                    background: desauratedBlue,
+                    color: "white",
+                    width: "100%",
+                  }
+                : {
+                    background: "white",
+                    color: desauratedBlue,
+                    width: "100%",
+                  }
+            }
+          >
+            {todoStatus === "completed" ? (
+              <div style={{ textDecoration: "line-through", color: "gray" }}>
+                {todoItem}
+              </div>
+            ) : (
+              <div>{todoItem}</div>
+            )}
+          </div>
+        </Paper>
+      </div>
+    );
+  };
+
+  return (
+    <div className={DarkMode ? "todo-body bg-dark" : "todo-body bg-light"}>
+      <nav>
+        <div>
+          <Button
+            style={{ float: "right" }}
+            color="inherit"
+            onClick={handleLogout}
+          >
+            <p>Logout</p>
+          </Button>
+        </div>
+      </nav>
+      <main>
+        <div className="todo-nav">
+          <h1>TODO</h1>
+          <Button onClick={() => setDarkMode(!DarkMode)}>
+            {" "}
+            {DarkMode ? <img src={moon}></img> : <img src={sun}></img>}
+          </Button>
+        </div>
+        <div>
+          <Paper
+            component="form"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "88%",
+              marginLeft: "35px",
+            }}
+            style={
+              !DarkMode
+                ? { background: desauratedBlue, color: "white" }
+                : { background: "white", color: desauratedBlue }
+            }
+          >
+            <IconButton sx={{ p: "10px" }} aria-label="menu">
+              <Checkbox
+                icon={DarkMode ? <CheckIconDark /> : <CheckIcon />}
+                disabled
+                sx={{
+                  color: pink[800],
+                  "&.Mui-hover": {
+                    color: pink,
+                  },
+                }}
+                style={
+                  !DarkMode
+                    ? {
+                        color: "white",
+                      }
+                    : {
+                        color: desauratedBlue,
+                      }
+                }
+              />
+            </IconButton>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Google Maps"
+              style={
+                !DarkMode
+                  ? {
+                      background: desauratedBlue,
+                      color: "white",
+                      fontFamily: "'Josefin Sans', 'sans-serif'",
+                    }
+                  : {
+                      background: "white",
+                      color: desauratedBlue,
+                      fontFamily: "'Josefin Sans', 'sans-serif'",
+                    }
+              }
+              onChange={(e) => setInputValue(e.target.value)}
+              value={InputValue}
+              onKeyDown={keyPress}
+            />
+          </Paper>
+        </div>
+        <div
+          className={
+            DarkMode ? "todo-container" : "todo-container todo-container-dark"
+          }
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+            }}
+          >
+            {Todos.map((todo) => {
+              //console.log(uid);
+              if (ActiveTodo && !CompletedTodo) {
+                if (todo[1].status === "active") {
+                  return (
+                    <ListItemContainer
+                      key={todo[1].id}
+                      todoItem={todo[1].item}
+                      todoStatus={todo[1].status}
+                      uid={todo[0]}
+                    />
+                  );
+                }
+              } else if (!ActiveTodo && CompletedTodo) {
+                if (todo[1].status === "completed") {
+                  return (
+                    <ListItemContainer
+                      key={todo[1].id}
+                      todoItem={todo[1].item}
+                      todoStatus={todo[1].status}
+                      uid={todo[0]}
+                    />
+                  );
+                }
+              } else {
+                return (
+                  <ListItemContainer
+                    key={todo[1].id}
+                    uid={todo[0]}
+                    todoItem={todo[1].item}
+                    todoStatus={todo[1].status}
+                  />
+                );
+              }
+            })}
+          </div>
+
+          <div
+            className={
+              !DarkMode
+                ? "todo-control-btn todo-control-btn-dark"
+                : "todo-control-btn"
+            }
+          >
+            <div>
+              {Todos.length == 1
+                ? Todos.length + " item left"
+                : Todos.length + " items left"}{" "}
+            </div>
+            <div className="todo-states">
+              <div
+                className={
+                  !ActiveTodo && !CompletedTodo
+                    ? "todo-state active-btn"
+                    : "todo-state"
+                }
+                id="all-todo"
+                onClick={() => {
+                  setActiveTodo(false);
+                  setCompletedTodo(false);
+                }}
+              >
+                All
+              </div>
+              <div
+                className={
+                  ActiveTodo && !CompletedTodo
+                    ? "todo-state active-btn"
+                    : "todo-state"
+                }
+                id="active-todo"
+                onClick={() => {
+                  setActiveTodo(true);
+                  setCompletedTodo(false);
+                }}
+              >
+                Active
+              </div>
+              <div
+                className={
+                  !ActiveTodo && CompletedTodo
+                    ? "todo-state active-btn"
+                    : "todo-state"
+                }
+                id="completed-todo"
+                onClick={() => {
+                  setActiveTodo(false);
+                  setCompletedTodo(true);
+                }}
+              >
+                Completed
+              </div>
+            </div>
+            <div>Clear Completed</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/component/hero.js
+++ src/component/hero.js
@@ -1,116 +1,15 @@
 import * as React from "react";
-import { useState ,useEffect} from "react";
-import Box from "@mui/material/Box";
+import { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
-
-import AddIcon from "@mui/icons-material/Add";
-import { styled, useTheme } from "@mui/material/styles";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-
 import Todo from "./Todo/Todo";
-const axios = require("axios");
-
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(9)} + 1px)`,
-  },
-});
-
-
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-
 
 const Hero = (props) => {
   const [active, setactive] = useState(null);
   const { handleLogout, setHasAccount } = props;
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  // useEffect(() => {
-  //   axios
-  //     .get("https://mocki.io/v1/c9a08268-1827-4c70-8497-8b6f2d928a3b")
-  //     .then(function (response) {
-  //       // handle success
-  //       console.log(response.data);
-  //       setactive(response.data);
-  //       //
-  //     });
-  // }, []);
 
-
-  //https://mocki.io/v1/c9a08268-1827-4c70-8497-8b6f2d928a3b
   return (
-    <div>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          open={open}
-          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        >
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <p> Mail Me</p>
-            </Typography>
-            <Button color="inherit" onClick={handleLogout}>
-              <p> Logout</p>
-            </Button>
-          </Toolbar>
-        </AppBar>
-        
-        <Box
-          style={{
-            padding: "3rem",
-            backgroundColor: "white",
-            minHeight: "100vh",
-            height: "auto",
-            overflow: "visible",
-            width: "100vw",
-          }}
-        >
-          <Toolbar />
-          <Todo todos={active} />
-        </Box>
-      </Box>
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: "white" }}>
+      <Todo handleLogout={handleLogout}></Todo>
     </div>
   );
 };
